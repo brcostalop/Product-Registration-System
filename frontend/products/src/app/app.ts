cc5ead0c75caf6ffb190860958b073a82bf017f1
brcostalop <bclopes008@gmail.com>
2025-07-25 00:04:44 -0300

    Update .gitignore

--- frontend/products/src/app/app.ts
+++ frontend/products/src/app/app.ts
@@ -11,19 +11,19 @@ import {JsonPipe} from '@angular/common';
 })
 export class App {
   protected readonly title = signal('products');
-  texto: any = '';
+  produtos: any = '';
   message = signal<string>('');
 
   constructor(private http: HttpClient) {
   }
 
   funcaoAPI() {
-    // this.http.get('http://localhost:8080/api/hello').subscribe({
-    this.http.post('http://localhost:8080/api/hello',{name: 'Bruno'}).subscribe({
+    this.http.get('http://localhost:8080/api/list').subscribe({
+    // this.http.post('http://localhost:8080/api/hello',{name: 'Bruno'}).subscribe({
     // this.http.put('http://localhost:8080/api/hello/123',{name: 'Bruno'}).subscribe({
     // this.http.delete('http://localhost:8080/api/hello/123').subscribe({
       next: (data) => {
-        this.texto = data;
+        this.produtos = data;
         console.log(data);
       },
       error: (err) => {
